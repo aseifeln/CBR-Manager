d19b81ed4ca83e54d37a3c19c9d6390761d1163c
Richard Zheng <rza47@sfu.ca>
2021-02-03 19:26:21 -0800

    Add basic info to client info page
    Just a layout, no actually backend work done. Only dummy links / buttons for now.

--- client/src/components/ClientInfo.js
+++ client/src/components/ClientInfo.js
@@ -1,24 +1,67 @@
 // The getWorkers request is temporary to ensure GET request is sent properly
+// To do: Add constructor and create class using correct prop when backend getClientInfo is implemented
 
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Button } from 'reactstrap';
 import { connect } from 'react-redux';
 import { getWorkers} from "../actions/workerActions";
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import tempLogo from './logo.jpeg'; // Temporary, will not push to repo
 
 class ClientInfo extends Component {
 
     componentDidMount() {
-        //console.log(this.props.match.params.id); // This is the id to send to backend
+        console.log(this.props.match.params.id); // This is the id to send to backend
         this.props.getWorkers();
-        //this.props.getClient(); Add proper GET request when backend is implemented
+        //this.props.getClientInfo(); Add proper GET request when backend is implemented
+    }
+
+    newVisit = (id) => {
+        console.log("New visit for client with id: " + id);
+        // POST request for new visit should go here
     }
 
     render() {
         const { workers } = this.props.worker;
-        console.log(workers);
         return(
-            "testing"
+            <Container>
+                <div class="page-header">
+                    <Container>
+                        <div class="row">
+                            <div class="col">
+                                <h1>Name</h1>
+                            </div>
+                            <div class="col">
+                                <div class="float-right"><Link to={"/client/" + this.props.match.params.id}>Edit Client Info</Link></div>
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="text-center">
+                            <img src={tempLogo} class="rounded-circle rounded"></img>
+                        </div>
+                    </div>
+                    <div class="col">
+                        Personal Info: 
+                        <ul class="list-unstyled">
+                            <li>- Location: </li>
+                            <li>- Age: </li>
+                            <li>- Gender: </li>
+                            <li>- Disability: </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col" align="center">
+                        <Button variant="primary" size="md" onClick={this.newVisit.bind(this, this.props.match.params.id)}>
+                            New Visit +
+                        </Button>
+                    </div>
+                </div>
+            </Container>
         );
     }
 }
