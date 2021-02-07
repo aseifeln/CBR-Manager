b5d2282f596e2385135765a5f90401cf1467742f
Christofer Calvin Kurniawan <ckurniaw@sfu.ca>
2021-02-06 17:54:25 -0800

    Convert password to be hashed password
    Hash the password to store in database using bcrypt

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
 const workers = require('./routes/workers');
+const { Pool } = require('pg');
 
 const app = express();
 const PORT = 5000;
@@ -22,7 +24,7 @@ app.get('/users/login', (req, res) => {
     res.render("login");
 });
 
-app.post('/users/register', (req, res) => {
+app.post('/users/register', async (req, res) => {
     let {name, location, worker_id, password, password2} = req.body;
 
     console.log({
@@ -45,6 +47,14 @@ app.post('/users/register', (req, res) => {
     }
     if(errors.length > 0){
         res.render('register', {errors});
+    } 
+    else{
+        //Hash the password to store in database
+        let hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+
+        //TODO: Check if user already exist
+
     }
 
 });
