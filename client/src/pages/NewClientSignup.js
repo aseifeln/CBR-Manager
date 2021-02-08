97280bb807687cf43177474e33f24e5c2d4e961d
Jason Yeung <contact@jasonyeung.me>
2021-02-07 17:13:29 -0800

    Add basic multi-step form functionality
    The form can move between different sections in using next/prev buttons.

--- client/src/pages/NewClientSignup.js
+++ client/src/pages/NewClientSignup.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Button, Form, FormGroup, Label, Input, FormText, Col, Row, Badge } from 'reactstrap';
 
 import AppNavbar from '../components/AppNavbar';
 
 function MultiStepForm({ children }) {
+  let [step, setStep] = useState(0)
+
+  // methods to move back & forth between sub-forms
+  const stepNext = () => setStep(Math.min(step+1, children.length-1))
+  const stepPrev = () => setStep(Math.max(step-1, 0))
+
+  const formContainerSize = {
+    margin: 'auto',
+    maxWidth: 600
+  }
+
   return (
-    <Container>
-      <div id="new-client" style={{margin: 'auto', maxWidth: 600}}>
-        <Badge color="primary" pill>Step 1 of 3</Badge>
-        {children}
+    <>
+      <Container>
+        <div style={formContainerSize}>
+          <Badge color="primary" pill>Step {step+1} of {children.length}</Badge>
+          <div>{children[step]}</div>
+
+          {/* dummy element just to add extra space */}
+          <div class='invisible py-4'>
+            <Button block></Button>
+          </div>
+        </div>
+      </Container>
+
+      <div class='fixed-bottom bg-light py-2'>
+        <Container>
+          <div class='d-flex justify-content-between align-items-center' style={formContainerSize}>
+            <div>Step {step+1} of {children.length}</div>
+
+            <div>
+              <Button color="primary" outline onClick={stepPrev}>Prev</Button>
+              &nbsp;
+
+              { (step+1 >= children.length) ? 
+                (<Button color="primary">Submit</Button>) : 
+                (<Button color="primary" onClick={stepNext}>Next</Button>)
+              }
+            </div>
+          </div>
+        </Container>
       </div>
-    </Container>
+    </>
   )
 }
 
@@ -140,8 +176,8 @@ function NewClientSignup() {
                   <Input type="text" name="age" placeholder="e.g. 123-123-1234" />
                 </FormGroup>
                 <FormGroup>
-                  <Button outline>Upload Client &amp; Caregiver Picture*</Button>
-                  <p>*This photo should include both the client and caregiver within the same frame</p>
+                  <Button outline>Upload Client &amp; Caregiver Picture</Button>
+                  <FormText>The photo should include both the client and caregiver within the same frame</FormText>
                 </FormGroup>
               </Col>
             </Col>
