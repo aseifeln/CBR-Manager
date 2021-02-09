eb6e762517e74a5e76fcc9c0efc3b29f1a483a89
hya62 <hansoo_yoon@sfu.ca>
2021-02-08 20:31:57 -0800

    Revert "Set up front end for dashboard"
    This reverts commit 52c467fd201acacdef75ad75954c4e55c950a6e2

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import OutRefs from '../components/OutRef';
-import PriorityClients from '../components/PriorityClients';
 import AppNavbar from '../components/AppNavbar';
-import '../css/Dashboard.css';
+import WorkerList from '../components/WorkerList';
 
 function Dashboard() {
   return (
     <div id="dashboard">
       <AppNavbar />
-        <h1>CBR Dashboard</h1>
-        <body>
-          <PriorityClients></PriorityClients>
-          <OutRefs></OutRefs>
-        </body>
+      <WorkerList 
+          workers={[]} 
+          getWorkers={()=>{}} 
+          addWorker={()=>{}}
+      />
+
       <Link to="/client/new">Link to new client</Link>
     </div>
   )
