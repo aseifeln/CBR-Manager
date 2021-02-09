15adb1863b786d401e04b6659645e05ff52c5c4f
Christofer Calvin Kurniawan <ckurniaw@sfu.ca>
2021-02-08 23:03:49 -0800

    Remove validation in registration and Add validation in login
    Login checks registered account to proceed

--- app.js
+++ app.js
@@ -7,58 +7,45 @@ const workers = require('./routes/workers');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
-app.set('view engine', 'ejs');
+const users = [];
+
 app.use(express.urlencoded({extended:false}));
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+app.get("/users/register", (req, res) => {
+    res.json(users);
 });
 
-app.get('/users/register', (req, res) => {
-    res.render("register");
+app.post("/users/register", async (req, res) => {
+    //TODO: Change variables after register layout finished
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { username: req.body.username, password: hashedPassword};
+        users.push(user);
+        res.status(201).send();
+    } catch {
+        res.status(500).send();
+    }
 });
 
-app.get('/users/login', (req, res) => {
-    res.render("login");
-});
-
-app.post('/users/register', async (req, res) => {
-    let {name, location, worker_id, password, password2} = req.body;
-
-    console.log({
-        name, 
-        location, 
-        worker_id,
-        password,
-        password2
-    });
-    
-    let errors = [];
-    if (!name || !location || !worker_id || !password || !password2){
-        errors.push({message: "All fields are required"});
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.username === req.body.username)
+    if( user == null ){
+        return res.status(400).send('All fields are required')
     }
-    if (password.length < 6){
-        errors.push({message: "Password must be at least 6 characters"});
-    }
-    if(password != password2){
-        errors.push({message:"Password and Confirm Password do not match"});
-    }
-    if(errors.length > 0){
-        res.render('register', {errors});
-    } 
-    else{
-        //Hash the password to store in database
-        let hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword);
-
-        //TODO: Check if user already exist
-
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('Login Success');
+        } else {
+            res.send('Wrong Password');
+        }
+    }catch{
+        res.status(500).send();
     }
 
 });
-
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
@@ -68,4 +55,4 @@ app.use('/workers', workers)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-module.exports = {app};
+//module.exports = {app};
