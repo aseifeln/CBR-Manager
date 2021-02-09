52c467fd201acacdef75ad75954c4e55c950a6e2
hya62 <hansoo_yoon@sfu.ca>
2021-02-08 19:57:31 -0800

    Set up front end for dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import OutRefs from '../components/OutRef';
+import PriorityClients from '../components/PriorityClients';
 import AppNavbar from '../components/AppNavbar';
-import WorkerList from '../components/WorkerList';
+import '../css/Dashboard.css';
 
 function Dashboard() {
   return (
     <div id="dashboard">
       <AppNavbar />
-      <WorkerList 
-          workers={[]} 
-          getWorkers={()=>{}} 
-          addWorker={()=>{}}
-      />
-
+        <h1>CBR Dashboard</h1>
+        <body>
+          <PriorityClients></PriorityClients>
+          <OutRefs></OutRefs>
+        </body>
       <Link to="/client/new">Link to new client</Link>
     </div>
   )
