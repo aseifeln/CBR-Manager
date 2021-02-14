53cf39c94117e8618736a8807f604d8e68ba900e
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-02-14 20:01:10 +0200

    Created an api endpoint to handle adding new clients to the database
    new clients can be added to the database by sending a post request to /clients/add

---
+++ routes/clients.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const client = require('../models/client')
+const multer = require('multer')
+const upload = multer({});
+
+// @route   POST /clients/add
+// @desc    POST Add a new client to the database
+router.post('/add', upload.single('Photo'), (req,res) => {
+    let {FirstName,LastName,Gender,Location,ContactNo,VillageNo,Age,DisabilityType,GPSLocation,Consent,
+        CaregiverState,CaregiverContactNo,HealthStatus,HealthDesc,HealthGoal,EducationStatus,EducationDesc,
+        EducationGoal,SocialStatus,SocialDesc,SocialGoal,WorkerId} = req.body;
+    client.create({
+        FirstName,
+        LastName,
+        Gender,
+        Location,
+        ContactNo,
+        VillageNo,
+        Age,
+        DisabilityType,
+        Photo: req.file.buffer,
+        GPSLocation,
+        Consent,
+        CaregiverState,
+        CaregiverContactNo,
+        HealthStatus,
+        HealthDesc,
+        HealthGoal,
+        EducationStatus,
+        EducationDesc,
+        EducationGoal,
+        SocialStatus,
+        SocialDesc,
+        SocialGoal,
+        WorkerId
+    })
+    .then(result => res.send("Client Added Successfully"))
+    .catch(err => res.json(err))
+})
+
+
+
+
+module.exports = router
