74786750f2b9c11081b3f981a073838f663a2b69
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-02-15 16:22:45 +0200

    Created an api endpoint to retrieve specific clients from the database
    A client with a specific id can be retrieved by sending a GET request to /clients/Id#

--- routes/clients.js
+++ routes/clients.js
@@ -4,6 +4,27 @@ const client = require('../models/client')
 const multer = require('multer')
 const upload = multer({});
 
+//Function that converts an image byte array into a base64 string
+//Reference: https://robert-keller22.medium.com/upload-and-download-images-to-a-postgres-db-node-js-92e43f232ae4
+function ConvertImage(client){
+    const clientImage = client.Photo.toString('base64')
+    client['Photo'] = clientImage
+}
+
+// @route   GET /clients/id
+// @desc    GET Retrieve a client with a certain id from the database
+router.get('/:id', (req,res) => {
+    const clientId = req.params.id
+    client.findByPk(clientId)
+        .then(client => {
+            ConvertImage(client)
+            return client;
+        })
+        .then(client => res.json(client))
+        .catch(err => res.status(400).json(err))
+})
+
+
 // @route   POST /clients/add
 // @desc    POST Add a new client to the database
 router.post('/add', upload.single('Photo'), (req,res) => {
