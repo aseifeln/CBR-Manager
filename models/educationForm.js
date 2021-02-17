8d55ea23a5178df5769035a595021995f7ed35ca
Danica Walton <danicaw@sfu.ca>
2021-02-17 11:58:57 -0800

    Adds model file for visit table and related tables

---
+++ models/educationForm.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const EducationForm = db.define('EducationForm', {
+    EducationFormId: {
+      type: Sequelize.UUID,
+      default: Sequelize.UUIDV4,
+      primaryKey: true
+    },
+    Advice: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    Advocacy: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    OrganizationReferral: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    Encouragement: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    GoalMet: {
+      type: Sequelize.ENUM,
+      values: ['Canceled', 'Ongoing', 'Concluded'],
+      allowNull: false
+    },
+    ConcludedOutcome: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    }
+}, {
+    tableName: 'EducationForm',
+    timestamps: false
+});
+
+module.exports = EducationForm;
