88932b10246c142a4105d51d22f4614269d330f7
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-02-19 15:56:28 +0200

    Added validation for the registration form
    User will get an error if a required field is left blank

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import { Button, Form, FormGroup, FormText, Input, Label } from 'reactstrap';
+import { Button, Form, FormGroup, FormFeedback, FormText, Input, Label } from 'reactstrap';
+
 
 import AppNavbar from "../components/AppNavbar";
 import "../css/SignUp.css";
@@ -14,18 +15,49 @@ function SignUpPage(props) {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const [firstNameErr, setFirstNameErr] = useState(false);
+    const [lastNameErr, setLastNameErr] = useState(false);
+    const [usernameErr, setUsernameErr] = useState(false);
+    const [photoErr, setPhotoErr] = useState(false);
+    const [passwordErr, setPasswordErr] = useState(false);
+    const [confirmPasswordErr, setConfirmPasswordErr] = useState(false);
+    
+    function initialErrState(){
+        setFirstNameErr(false)
+        setLastNameErr(false)
+        setUsernameErr(false)
+        setPhotoErr(false)
+        setPasswordErr(false)
+        setConfirmPasswordErr(false)
+    }
     function handleSubmit(event) {
         event.preventDefault();
+        initialErrState();
 
         if (authPasses()) {
             props.history.push("/");
             return;
         }
-        alert("Username or password is invalid size");
+        //alert("Username or password is invalid size");
     }
 
-    function authPasses() {
-        return username.length > 0 && password.length > 0;
+    function authPasses() { 
+        if(!firstName.length > 0){
+            setFirstNameErr(true)
+        }
+        if(!lastName.length > 0){
+            setLastNameErr(true)
+        }
+        if(!username.length > 0){
+            setUsernameErr(true)
+        }
+        if(!password.length > 0){
+            setPasswordErr(true)
+        }
+        if(!confirmPassword.length > 0 || (confirmPassword !== password)){
+            setConfirmPasswordErr(true)
+        }
+        //return true;
     }
 
     return (
@@ -35,32 +67,36 @@ function SignUpPage(props) {
                 <h2><b>Create CRB Account</b></h2>
                 <FormGroup>
                     <Label for="firstName">First name</Label>
-                    <Input type="text"
+                    <Input invalid={firstNameErr} type="text"
                         id="firstName"
                         value={firstName}
                         onChange={(event) => setFirstName(event.target.value)}
-                        placeholder="First name" />
+                        placeholder="First name"/>
+                    <FormFeedback>Please enter your first name!</FormFeedback>
                 </FormGroup>
                 <FormGroup>
                     <Label for="lastName">Last name</Label>
-                    <Input type="text"
+                    <Input invalid={lastNameErr} type="text"
                         id="lastName"
                         value={lastName}
                         onChange={(event) => setLastName(event.target.value)}
-                        placeholder="Last name" />
+                        placeholder="Last name"/>
+                    <FormFeedback>Please enter your last name!</FormFeedback>
                 </FormGroup>
                 <FormGroup>
                     <Label for="userName">Username</Label>
-                    <Input type="text"
+                    <Input invalid={usernameErr} type="text"
                         id="userName"
                         value={username}
                         onChange={(event) => setUsername(event.target.value)}
                         placeholder="Username" />
+                    <FormFeedback>Please enter a username!</FormFeedback>
                     <FormText><i>Add username to easily log in</i></FormText>
                 </FormGroup>
                 <FormGroup>
                     <Label for="profilePhoto">Profile Picture</Label>
-                    <Input type="file" name="profilePhoto" id="profilePhoto" />
+                    <Input invalid={photoErr} type="file" name="profilePhoto" id="profilePhoto" />
+                    <FormFeedback>Please upload a profile picture!</FormFeedback>
                 </FormGroup>
                 <FormGroup>
                     <Label for="location">Location</Label>
@@ -68,26 +104,32 @@ function SignUpPage(props) {
                         id="location"
                         value={location}
                         onChange={(event) => setLocation(event.target.value)}>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                        <option>BidiBidi Zone 1</option>
+                        <option>BidiBidi Zone 2</option>
+                        <option>BidiBidi Zone 3</option>
+                        <option>BidiBidi Zone 4</option>
+                        <option>BidiBidi Zone 5</option>
+                        <option>Palorinya Basecamp</option>
+                        <option>Palorinya Zone 1</option>
+                        <option>Palorinya Zone 2</option>
+                        <option>Palorinya Zone 3</option>
                     </Input>
                     <FormText><i>Assigned zone. This can be changed later</i></FormText>
                 </FormGroup>
                 <FormGroup>
                     <Label  for="password">Password</Label>
-                    <Input type="text"
+                    <Input invalid={passwordErr} type="password"
                         id="password"
                         value={password}
                         onChange={(event) => setPassword(event.target.value)}
                         placeholder="Password" />
-                    <Input type="text"
+                    <FormFeedback>Password must have more than 5 characters</FormFeedback>
+                    <Input invalid={confirmPasswordErr} type="password"
                         id="confirmPassword"
                         value={confirmPassword}
                         onChange={(event) => setConfirmPassword(event.target.value)}
                         placeholder="Confirm password" />
+                    <FormFeedback>Passwords don't match</FormFeedback>
                 </FormGroup>
                 <Button
                     type="submit" id="submitBtn" onClick={handleSubmit}>Create Account</Button>
