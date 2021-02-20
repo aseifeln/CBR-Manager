a865f9d65b98221c4d608fcdc0c2f47f464ee993
hya62 <hansoo_yoon@sfu.ca>
2021-02-19 17:54:36 -0800

    Clicking on list items takes user to client info page

--- client/src/components/PriorityClients.js
+++ client/src/components/PriorityClients.js
@@ -42,7 +42,7 @@ function PriorityClients(){
                 <ListGroup>
                     {data.length>0?data.map((client)=>{
                         return(
-                        <ListGroupItem key={client.ClientId}>
+                        <ListGroupItem key={client.ClientId} tag="a" href={`/client/${client.ClientId}`} action>
                             <ListGroupItemHeading> {client.FirstName} {client.LastName} </ListGroupItemHeading>
                             <ListGroupItemHeading> ID: {client.ClientId}</ListGroupItemHeading>
                             <ListGroupItemText>
