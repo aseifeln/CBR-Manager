3afc1bf02eb77ce37123d6c46b2db0ebbf8c726f
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-02-21 01:43:41 +0200

    Added validation for the login form
    Form shows an error message if the user attempts to login without entering a username or password

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, FormFeedback, Label, Input } from 'reactstrap';
 
 import AppNavbar from "../components/AppNavbar";
 import "../css/Login.css";
@@ -10,40 +10,63 @@ function Login(props) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const[usernameErr, setUsernameErr] = useState(false);
+    const[passwordErr, setPasswordErr] = useState(false);
+
+    function initialErrState(){
+        setUsernameErr(false)
+        setPasswordErr(false)
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
+        initialErrState();
 
         if (authPasses()) {
             props.history.push("/");
             return;
         }
-        alert("Username or password is invalid size");
     }
 
     function authPasses() {
-        return username.length > 0 && password.length > 0;
+        let pass = true
+        if(!username.length > 0){
+            setUsernameErr(true)
+            pass = false
+        }
+        if(!password.length > 0){
+            setPasswordErr(true)
+            pass = false
+        }
+        return pass
     }
 
     return (
         <div className='Login'>
             <AppNavbar />
             <Form onSubmit={handleSubmit}>
-                <Label>Username: </Label>
-                <Input
-                    type="text"
-                    required
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                    placeholder="Username"
-                />
-                <Label>Password: </Label>
+                <FormGroup>
+                    <Label>Username: </Label>
                     <Input
-                    type="password"
-                    required
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    placeholder="Password"
-                />
+                        type="text"
+                        invalid={usernameErr}
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        placeholder="Username"
+                    />
+                    <FormFeedback>Please enter your username!</FormFeedback>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Password: </Label>
+                        <Input
+                        type="password"
+                        invalid={passwordErr}
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        placeholder="Password"
+                    />
+                    <FormFeedback>Please enter your password!</FormFeedback>
+                </FormGroup>
                 <Button type="submit" onClick={handleSubmit}>Login</Button>
                 <Link to="/signup">Create Account</Link>
             </Form>
