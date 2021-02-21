0ca46f70c997f6b3c81f985f13e4ee07078803a6
Richard Zheng <rza47@sfu.ca>
2021-02-20 18:19:26 -0800

    Change configs for production
    Should be usable with Heroku now

--- config/database.js
+++ config/database.js
@@ -2,4 +2,22 @@
 const { Sequelize } = require('sequelize');
 
 //Connecting to DB
-module.exports = new Sequelize('postgres://' + process.env.DB_URL)
+
+// Reference: https://medium.com/@andrewoons/how-to-define-sequelize-associations-using-migrations-de4333bf75a7
+if (process.env.NODE_ENV === 'production')
+{
+    module.exports = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
+}
+else 
+{
+    module.exports = new Sequelize('postgres://' + process.env.DB_URL)
+}
