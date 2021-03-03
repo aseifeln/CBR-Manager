c9d755a53369a767d8d4540d8fe1bdd17f363672
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-03-03 19:35:39 +0200

    Add optional chaining for nested object properties

--- client/src/pages/VisitInfo.js
+++ client/src/pages/VisitInfo.js
@@ -98,91 +98,97 @@ function VisitInfo(props) {
                     <Col><h1>Visit Details:</h1></Col>
                 </Row>
             </Container>
-            {visit.HealthForm ?
+            {visit.HealthForm !== null ?
             <Container>
                 <Row>
                     <Col><h2>Health</h2></Col>
                 </Row>
                 <ListGroup>
-                    {visit.HealthForm.Wheelchair ?
+                    {visit.HealthForm?.Wheelchair ?
                     <ListGroupItem><b>Wheelchair: </b><br/>{visit.HealthForm.Wheelchair}</ListGroupItem>
                     : ""}
-                    {visit.HealthForm.Prosthetic ?
+                    {visit.HealthForm?.Prosthetic ?
                     <ListGroupItem><b>Prosthetic: </b><br/>{visit.HealthForm.Prosthetic}</ListGroupItem>
                     : ""}
-                    {visit.HealthForm.Orthotic ?
+                    {visit.HealthForm?.Orthotic ?
                     <ListGroupItem><b>Orthotic: </b><br/>{visit.HealthForm.Orthotic}</ListGroupItem>
                     : ""}
-                    {visit.HealthForm.WheelchairRepair ? 
+                    {visit.HealthForm?.WheelchairRepair ? 
                     <ListGroupItem><b>Wheelchair Repair: </b><br/>{visit.HealthForm.WheelchairRepair}</ListGroupItem>
                     : ""}
-                    {visit.HealthForm.HealthCenterReferral ? 
+                    {visit.HealthForm?.HealthCenterReferral ? 
                     <ListGroupItem><b>Health Centre Referral: </b><br/>{visit.HealthForm.HealthCenterReferral}</ListGroupItem>
                     : ""}
-                    {visit.HealthForm.Advice ? 
+                    {visit.HealthForm?.Advice ? 
                     <ListGroupItem><b>Advice: </b><br/>{visit.HealthForm.Advice}</ListGroupItem>
                     : ""}
-                    {visit.HealthForm.Advocacy ?
+                    {visit.HealthForm?.Advocacy ?
                     <ListGroupItem><b>Advocacy: </b><br/>{visit.HealthForm.Advocacy}</ListGroupItem>
                     : ""}
-                    {visit.HealthForm.Encouragement ?
+                    {visit.HealthForm?.Encouragement ?
                     <ListGroupItem><b>Encouragement: </b><br/>{visit.HealthForm.Encouragement}</ListGroupItem>
                     : ""}
+                    {visit.HealthForm?.GoalMet ?
                     <ListGroupItem><b>Goal Met? </b><br/>{visit.HealthForm.GoalMet}</ListGroupItem>
-                    {visit.HealthForm.GoalMet === "Concluded" ?
+                    : ""}
+                    {visit.HealthForm?.GoalMet === "Concluded" ?
                     <ListGroupItem><b>Concluded Outcome</b><br/>{visit.HealthForm.ConcludedOutcome}</ListGroupItem>
                     : ""}
                 </ListGroup>
             </Container>
             : ""}
             <br/>
-            {visit.EducationForm ? 
+            {visit.EducationForm !== null ? 
             <Container>
                 <Row>
                     <Col><h2>Education</h2></Col>
                 </Row>
                 <ListGroup>
-                    {visit.EducationForm.Advice ?
+                    {visit.EducationForm?.Advice ?
                     <ListGroupItem><b>Advice: </b><br/>{visit.EducationForm.Advice}</ListGroupItem>
                     : ""}
-                    {visit.EducationForm.Advocacy ?
+                    {visit.EducationForm?.Advocacy ?
                     <ListGroupItem><b>Advocacy: </b><br/>{visit.EducationForm.Advocacy}</ListGroupItem>
                     : ""}
-                    {visit.EducationForm.Encouragement ?
+                    {visit.EducationForm?.Encouragement ?
                     <ListGroupItem><b>Encouragement: </b><br/>{visit.EducationForm.Encouragement}</ListGroupItem>
                     : ""}
-                    {visit.EducationForm.OrganizationReferral ?
+                    {visit.EducationForm?.OrganizationReferral ?
                     <ListGroupItem><b>Organization Referral: </b><br/>{visit.EducationForm.OrganizationReferral}</ListGroupItem>
                     : ""}
-                    <ListGroupItem><b>Goal Met? </b><br/>{visit.HealthForm.GoalMet}</ListGroupItem>
-                    {visit.EducationForm.GoalMet === "Concluded" ?
-                    <ListGroupItem><b>Concluded Outcome</b><br/>{visit.HealthForm.ConcludedOutcome}</ListGroupItem>
+                    {visit.EducationForm?.GoalMet ?
+                    <ListGroupItem><b>Goal Met? </b><br/>{visit.EducationForm.GoalMet}</ListGroupItem>
+                    : ""}
+                    {visit.EducationForm?.GoalMet === "Concluded" ?
+                    <ListGroupItem><b>Concluded Outcome</b><br/>{visit.EducationForm.ConcludedOutcome}</ListGroupItem>
                     : ""}
                 </ListGroup>
             </Container>
             : ""}
             <br/>
-            {visit.SocialForm ?
+            {visit.SocialForm !== null ?
             <Container>
                 <Row>
                     <Col><h2>Social</h2></Col>
                 </Row>
                 <ListGroup>
-                    {visit.SocialForm.Advice ?
+                    {visit.SocialForm?.Advice ?
                     <ListGroupItem><b>Advice: </b><br/>{visit.SocialForm.Advice}</ListGroupItem>
                     : ""}
-                    {visit.SocialForm.Advocacy ?
+                    {visit.SocialForm?.Advocacy ?
                     <ListGroupItem><b>Advocacy: </b><br/>{visit.SocialForm.Advocacy}</ListGroupItem>
                     : ""}
-                    {visit.SocialForm.Encouragement ?
+                    {visit.SocialForm?.Encouragement ?
                     <ListGroupItem><b>Encouragement: </b><br/>{visit.SocialForm.Encouragement}</ListGroupItem>
                     : ""}
-                    {visit.SocialForm.OrganizationReferral ?
+                    {visit.SocialForm?.OrganizationReferral ?
                     <ListGroupItem><b>Organization Referral: </b><br/>{visit.SocialForm.OrganizationReferral}</ListGroupItem>
                     : ""}
+                    {visit.SocialForm?.GoalMet ?
                     <ListGroupItem><b>Goal Met? </b><br/>{visit.SocialForm.GoalMet}</ListGroupItem>
-                    {visit.SocialForm.GoalMet === "Concluded" ?
-                    <ListGroupItem><b>Concluded Outcome</b><br/>{visit.HealthForm.ConcludedOutcome}</ListGroupItem>
+                    : ""}
+                    {visit.SocialForm?.GoalMet === "Concluded" ?
+                    <ListGroupItem><b>Concluded Outcome</b><br/>{visit.SocialForm.ConcludedOutcome}</ListGroupItem>
                     : ""}
                 </ListGroup>
             </Container>
