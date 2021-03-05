47f5fe1da7b7da7686916c28c634ff6711d1a470
Christofer Calvin Kurniawan <ckurniaw@sfu.ca>
2021-03-04 21:19:18 -0800

    Redirect page logout
    Go to login page when logout

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,11 +1,12 @@
 import React,{useEffect} from 'react';
 import axios from 'axios'
 
-function Logout() {
+function Logout(props) {
     function logout() {
         axios.post('/users/logout')
             .then(res => {
                 console.log(res);
+                props.history.push("/");
             })
             .catch(err => {
                 console.log(err);
