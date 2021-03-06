ddfa771e7f582f3402fb1528f92297f10d02de5f
Richard Zheng <rza47@sfu.ca>
2021-03-06 14:20:29 -0800

    Add general info form
    Made very similarly to the NewVisit General Info page using the MultiStepForm

--- client/src/pages/NewReferral.js
+++ client/src/pages/NewReferral.js
@@ -8,8 +8,10 @@ import NotFoundPage from './404';
 function NewReferral(props) {
 
     const history = useHistory();
+
     const [ client, setClient ] = useState({});
     const [ clientFound, setClientFound ] = useState(false);
+    const [ otherSelected, setOtherSelected ] = useState(false);
 
     useEffect(() => {
 
@@ -37,9 +39,93 @@ function NewReferral(props) {
     }    
     
     return (
-        <div>
-            New Referral
-        </div>
+        <Container>
+            <Row>
+              <Col className="font-weight-bold" style={{fontSize: "30px"}}>
+                Client: {client.FirstName + ' ' + client.LastName}
+              </Col>
+              <Col>
+                <Button variant="primary" size="md" className="float-right">
+                  Save Referral
+                </Button>
+              </Col>
+            </Row>
+
+            <MultiStepForm name="New Referral">
+                <Step name="General Info">
+
+                    <Row form>
+                        <Col>
+                            <FormGroup>
+                                <FieldInput placeholder="Autofill CBR worker Name" name="worker" label="CBR Worker"/>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    <Row form>
+                        <Col>
+                            <FormGroup>
+                                <FieldInput type="date" name="date" label="Date:" required="Date required" defaultValue={(new Date()).toLocaleDateString('en-CA')}/>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Label>
+                                    Service required (Select all that apply)
+                                </Label>
+                            </Col>
+                        </Row>
+                    
+                        <Row>
+                            <Col>
+                                <Label check style={{paddingLeft: "21px", paddingRight: "20px"}}>
+                                    <Input type="checkbox" name="wheelCheckBox"/>
+                                    Wheelchair
+                                </Label>
+                                <Label check style={{paddingLeft: "21px", paddingRight: "20px"}}>
+                                    <Input type="checkbox" name="physioCheckBox"/>
+                                    Physiotheraphy
+                                </Label>
+                                <Label check style={{paddingLeft: "21px", paddingRight: "20px"}}>
+                                    <Input type="checkbox" name="prostheticCheckBox"/>
+                                    Prosthetic
+                                </Label>
+                                <Label check style={{paddingLeft: "21px", paddingRight: "20px"}}>
+                                    <Input type="checkbox" name="orthoticCheckBox"/>
+                                    Orthotic
+                                </Label>
+                                <Label check style={{paddingLeft: "21px", paddingRight: "20px"}}>
+                                    <Input type="checkbox" name="orthoticCheckBox" onChange={() => setOtherSelected(!otherSelected)}/>
+                                    Other
+                                </Label>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+
+                    {(otherSelected) ? (
+                        <Row form>
+                            <Col>
+                                <FormGroup>
+                                    <FieldInput type="textarea" placeholder="Please describe" name="otherServiceDesc" label="Other Service" required="Other service description is required"/>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                    ) : ""}
+
+                    <Row form>
+                        <Col>
+                            <FormGroup>
+                                <FieldInput placeholder="Center Services" name="referTo" label="Refer to" required="Center Services is required"/>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                </Step>
+            </MultiStepForm>
+        </Container>
     )
 }
 
