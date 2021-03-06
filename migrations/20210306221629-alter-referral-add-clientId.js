f9e4e47b9c5e13aa58fe73fe073840524702fdfa
Danica Walton <danicaw@sfu.ca>
2021-03-06 14:38:47 -0800

    Removes unnecessary comments from alter referral migration

--- migrations/20210306221629-alter-referral-add-clientId.js
+++ migrations/20210306221629-alter-referral-add-clientId.js
@@ -2,23 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
 
      await queryInterface.addColumn(
         'Referral', 'ClientId', {type: Sequelize.UUID} );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    // intentionally blank
   }
 };
