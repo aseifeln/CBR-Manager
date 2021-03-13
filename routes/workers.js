0a905f403cafafcd5683370c53722a8e1384d525
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-03-13 15:48:10 +0200

    Adds API to retrieve all visits performed by a worker

--- routes/workers.js
+++ routes/workers.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const worker = require('../models/worker')
+const visit = require('../models/visit')
 
 // @route   GET /workers
 // @desc    GET Retrieve all workers from the database
@@ -14,6 +15,25 @@ router.get('/', (req,res) => {
     .catch(err => res.status(404).json(err))
 })
 
+// @route   GET /workers/id/visits
+// @desc    GET Retrieve all visits done by a worker from the database
+router.get('/:id/visits', (req,res) => {
+    const workerId = req.params.id;
+
+    visit.findAll({
+        attributes: [
+            'ClientId', 'VisitId', 'VisitPurpose', 'Date'
+        ],
+        where: {
+            WorkerId: workerId
+        },
+        order: [
+            ['Date', 'DESC']
+        ]
+    })
+    .then(visits => res.json(visits))
+    .catch(err => res.status(404).json(err))
+})
 
 
 module.exports = router
