9c1ee32de2056df83d387f5461c6c3563a81895f
David Seiler <dseiler@sfu.ca>
2021-03-18 20:53:36 -0700

    Setup database and scripts for running tests.

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ db.authenticate()
 
 // Heroku will use some other port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-module.exports = {app};
+module.exports = { app, server };
 
 
