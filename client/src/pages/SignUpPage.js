7aedab9660efa66ca95a417365e37c0fc2d3a554
Christofer Calvin Kurniawan <ckurniaw@sfu.ca>
2021-03-18 23:10:55 -0700

    Fix spacing

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,12 +1,8 @@
 import React, { useState,useEffect } from "react";
 import { Link } from 'react-router-dom';
-import axios from 'axios'
 import { Button, Form, FormGroup, FormFeedback, FormText, Input, Label } from 'reactstrap';
-
-
+import axios from 'axios'
 import "../css/SignUp.css";
-
-
 
 function SignUpPage(props) {
 
