e2532386ffd625385ef06ce078d29bd581827768
David Seiler <dseiler@sfu.ca>
2021-03-29 12:57:06 -0700

    Updated baseline survey migration to properly reference individual surveys

---
+++ migrations/20210329195132-create-baselineSurvey.js
@@ -0,0 +1,84 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BaselineSurvey',{
+      BaselineSurveyId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      HealthSurveyId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'HealthSurvey',
+          key: 'HealthSurveyId'
+        },
+        allowNull: true
+      },
+      SocialSurveyId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'SocialSurvey',
+          key: 'SocialSurveyId'
+        },
+        allowNull: true
+      },
+      ShelterSurveyId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'ShelterSurvey',
+          key: 'ShelterSurveyId'
+        },
+        allowNull: true
+      },
+      NutritionSurveyId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'NutritionSurvey',
+          key: 'NutritionSurveyId'
+        },
+        allowNull: true
+      },
+      LivelihoodSurveyId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'LivelihoodSurvey',
+          key: 'LivelihoodSurveyId'
+        },
+        allowNull: true
+      },
+      EmpowermentSurveyId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'EmpowermentSurvey',
+          key: 'EmpowermentSurveyId'
+        },
+        allowNull: true
+      },
+      EducationSurveyId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'EducationSurvey',
+          key: 'EducationSurveyId'
+        },
+        allowNull: true
+      },
+      Date: {
+        type: Sequelize.DATEONLY,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      },
+      DateEdited: {
+        type: Sequelize.DATEONLY,
+        defaultValue: Sequelize.NOW,
+        allowNull: true
+      },
+
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BaselineSurvey');
+  }
+};
