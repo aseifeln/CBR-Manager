94f16eb829c98e06a735c516b50f8cc495889e61
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-03-29 19:48:44 +0200

    Adds a form to create admin accounts from the dashboard

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { Container} from 'reactstrap';
+import React, {useState} from 'react';
+import { Container, Button} from 'reactstrap';
 import AdminSideBar from '../components/AdminSideBar';
 import CookieChecker from '../components/CookieChecker';
+import CreateAdminAccount from '../components/CreateAdminAccount';
 
 function AdminDashboard() {
+
+    const [toggleCreateAdmin, setToggleCreateAdmin] = useState(false);
 
     return(
         <>
         <CookieChecker></CookieChecker>
         <AdminSideBar/>
+        {toggleCreateAdmin ?
+        <CreateAdminAccount onClick={() => setToggleCreateAdmin(false)}
+        onSuccess={() => setToggleCreateAdmin(false)}/>
+        : ''}
         <Container>
             <div className="main-content">
                 <h1>Dashboard</h1>
+                <Button onClick={() => setToggleCreateAdmin(true)}>Create New Admin</Button>
             </div>
         </Container>
         </>
