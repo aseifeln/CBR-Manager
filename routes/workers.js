694ff26f522c57395f5a995949166957b0386b9b
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-03-30 20:56:56 +0200

    Add APIs to retrieve cbr worker statistics

--- routes/workers.js
+++ routes/workers.js
@@ -4,6 +4,8 @@ const worker = require('../models/worker')
 const visit = require('../models/VisitForms/visit')
 const referral = require('../models/ReferralForms/referral')
 const Client = require('../models/client')
+const { Op } = require('sequelize')
+const Sequelize = require('sequelize')
 
 // @route   GET /workers
 // @desc    GET Retrieve all workers from the database
@@ -75,6 +77,163 @@ router.get('/:id/referrals', (req,res) => {
         ]
     })
     .then(referrals => res.json(referrals))
+    .catch(err => res.status(400).json(err))
+})
+// @route   GET /workers/workerId/clients/count
+// @desc    GET Retrieve the number of all clients created by a worker
+router.get('/:id/clients/count', (req, res) => {
+    const workerId = req.params.id
+
+    Client.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId
+        }
+    })
+    .then(count => res.json(count))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET /workers/workerId/clients/weeklyCount
+// @desc    GET Retrieve the number of all clients created by a worker in the past week
+router.get('/:id/clients/weeklyCount', (req, res) => {
+    const workerId = req.params.id
+
+    let fromDate = new Date()
+    fromDate.setHours(0,0,0,0)
+
+    let toDate = new Date()
+    toDate.setHours(0,0,0,0)
+
+    Client.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId,
+            DateCreated: {
+                [Op.between]: [fromDate - ((24 * 60 * 60 * 1000) * 7), toDate]
+            }
+        }
+    })
+    .then(count => res.json(count))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET /workers/workerId/visits/count
+// @desc    GET Retrieve the number of all visits created by a worker
+router.get('/:id/visits/count', (req, res) => {
+    const workerId = req.params.id
+
+    visit.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId
+        }
+    })
+    .then(count => res.json(count))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET /workers/workerId/visits/weeklyCount
+// @desc    GET Retrieve the number of all visits created by a worker in the past week
+router.get('/:id/visits/weeklyCount', (req, res) => {
+    const workerId = req.params.id
+
+    let fromDate = new Date()
+    fromDate.setHours(0,0,0,0)
+
+    let toDate = new Date()
+    toDate.setHours(0,0,0,0)
+
+    visit.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId,
+            Date: {
+                [Op.between]: [fromDate - ((24 * 60 * 60 * 1000) * 7), toDate]
+            }
+        }
+    })
+    .then(count => res.json(count))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET /workers/workerId/referrals/count
+// @desc    GET Retrieve the number of all referrals created by a worker
+router.get('/:id/referrals/count', (req, res) => {
+    const workerId = req.params.id
+
+    referral.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId
+        }
+    })
+    .then(count => res.json(count))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET /workers/workerId/referrals/weeklyCount
+// @desc    GET Retrieve the number of all referrals created by a worker in the past week
+router.get('/:id/referrals/weeklyCount', (req, res) => {
+    const workerId = req.params.id
+
+    let fromDate = new Date()
+    fromDate.setHours(0,0,0,0)
+
+    let toDate = new Date()
+    toDate.setHours(0,0,0,0)
+
+    referral.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId,
+            Date: {
+                [Op.between]: [fromDate - ((24 * 60 * 60 * 1000) * 7), toDate]
+            }
+        }
+    })
+    .then(count => res.json(count))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET /workers/workerId/referrals/resolved/count
+// @desc    GET Retrieve the number of all referrals resolved by a worker
+router.get('/:id/referrals/resolved/count', (req, res) => {
+    const workerId = req.params.id
+
+    referral.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId,
+            Status: 'Resolved'
+        }
+    })
+    .then(count => res.json(count))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET /workers/workerId/referrals/resolved/weeklyCount
+// @desc    GET Retrieve the number of all referrals resolved by a worker in the past week
+router.get('/:id/referrals/resolved/weeklyCount', (req, res) => {
+    const workerId = req.params.id
+
+    let fromDate = new Date()
+    fromDate.setHours(0,0,0,0)
+
+    let toDate = new Date()
+    toDate.setHours(0,0,0,0)
+
+    referral.count({
+        col: 'WorkerId',
+        where: {
+            WorkerId: workerId,
+            Status: 'Resolved',
+            Date: {
+                [Op.between]: [fromDate - ((24 * 60 * 60 * 1000) * 7), toDate]
+            }
+        }
+    })
+    .then(count => res.json(count))
     .catch(err => res.status(400).json(err))
 })
 
