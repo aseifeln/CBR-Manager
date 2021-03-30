d8db1f411cb51302ea18b3f25a297205c72a2e66
Danica Walton <danicaw@sfu.ca>
2021-03-29 23:02:41 -0700

    Creates alert migration file and model file

---
+++ migrations/20210330053831-create-alert.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    queryInterface.createTable('Alert', {
+    AlertId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    Title: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    Message: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    Date: {
+      type: Sequelize.DATE,
+      default: Sequelize.NOW,
+      allowNull: false
+    },
+    SpecificWorkers: {
+      type: Sequelize.ARRAY(Sequelize.INTEGER),
+      allowNull: true
+    },
+    ForAllWorkers: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    },{
+      tableName: 'Alert',
+      timestamps: false
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Alert');
+  }
+};
