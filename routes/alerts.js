b70df307e81d4e12a66f0706832304a2ca7019fc
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-04-05 00:46:18 +0200

    Adds transaction functionality to APIs

--- routes/alerts.js
+++ routes/alerts.js
@@ -1,45 +1,90 @@
 const express = require('express')
 const router = express.Router()
 const alert = require('../models/alert')
+const worker = require('../models/worker')
 const { Op } = require('sequelize')
 const {sequelize} = require('../models/alert')
 
 // @route   GET /alerts/id
 // @desc    GET Retrieve an alert by id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const alertId = req.params.id
 
-    alert.findByPk(alertId)
-    .then(alert => res.json(alert))
-    .catch(err => res.status(404).json(err))
+    let transaction
+
+    try {
+        transaction = await sequelize.transaction();
+
+        let result = await alert.findByPk(alertId, { transaction })
+
+        if(result === null) {
+            throw new Error("Alert not found") 
+        }
+
+        await transaction.commit();
+        res.status(200).json(result)
+        
+    } catch (error) {
+        await transaction.rollback();
+        if(error.message === "Alert not found") {
+            res.status(404).json(error.message)
+        }
+        else {
+            res.status(500).json(error.message);
+        }
+    }
 })
 
 // @route   GET /alerts/worker/id
 // @desc    GET Retrieve all alerts for a specific cbr worker
-router.get('/worker/:id', (req, res) => {
+router.get('/worker/:id', async (req, res) => {
     const workerId = req.params.id
 
-    alert.findAll({
-        attributes: [
-            'AlertId',
-            'Title',
-            'Message',
-            'Date',
-            'AuthorUsername'
-        ],
-        where: {
-            [Op.or]: [
-                { ForAllWorkers: true },
-                { 
-                  SpecificWorkers: {
-                    [Op.contains]: [workerId]
-                  } 
-                }
-            ]
+    let transaction
+
+    try {
+        transaction = await sequelize.transaction();
+
+        let isWorker = await worker.findByPk(workerId, { transaction })
+
+        if(isWorker === null) {
+            throw new Error("Worker not found") 
         }
-    })
-    .then(alert => res.json(alert))
-    .catch(err => res.status(404).json(err))
+        else{
+            let result = await alert.findAll({
+                attributes: [
+                    'AlertId',
+                    'Title',
+                    'Message',
+                    'Date',
+                    'AuthorUsername'
+                ],
+                where: {
+                    [Op.or]: [
+                        { ForAllWorkers: true },
+                        { 
+                          SpecificWorkers: {
+                            [Op.contains]: [workerId]
+                          } 
+                        }
+                    ]
+                }
+            }, { transaction })
+    
+            await transaction.commit();
+            res.status(200).json(result)
+        }
+    } 
+    catch (error) {
+        await transaction.rollback();
+        if(error.message === "Worker not found") {
+            res.status(404).json(error.message)
+        }
+        else {
+            res.status(500).json(error.message);
+        }
+    }
+
 })
 
 
