717514d152cf1e810f6931facc797efdefb17771
Jason Yeung <jason_yeung_4@sfu.ca>
2021-04-05 16:27:13 -0700

    Apply 1 suggestion(s) to 1 file(s)

--- client/src/components/BaselineSurvey.js
+++ client/src/components/BaselineSurvey.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'reactstrap';
 import {Table} from 'reactstrap';
 import axios from 'axios';
