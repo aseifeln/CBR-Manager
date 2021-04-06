986af282f38d7342003bda331839c195adf2f24f
Richard Zheng <rza47@sfu.ca>
2021-04-06 13:26:09 -0700

    Add accordion for ref stat
    Uses similar code to accordion in client info page

--- client/src/pages/AdminPages/AdminInsights.js
+++ client/src/pages/AdminPages/AdminInsights.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Container, Row } from 'reactstrap';
+import React, { useState } from 'react';
+import { Button, Card, CardHeader, CardBody, Collapse, Row, Col } from 'reactstrap';
 import AdminSideBar from '../../components/AdminSideBar';
 import CookieChecker from '../../components/CookieChecker';
 import ReferralStatistics from '../../components/statistics/ReferralStatistics';
 
 function AdminInsights() {
+
+    const [ showRefStats, setShowRefStats ] = useState(false);
+    const statFontSize = {color:"white",fontSize: "20px", fontWeight: "bold"};
 
     return(
         <>
@@ -14,7 +17,23 @@ function AdminInsights() {
 
                 <div className='admin-container'>
                     <h1>Insights</h1>
-                    <ReferralStatistics/>
+                    <Card>
+                        <CardHeader>
+                            <Row>
+                                <Col><h2 style={{statFontSize}}>Referral Stats</h2></Col>
+                                <Col>
+                                    <Button variant="primary" size="md" className="float-right" onClick={() => setShowRefStats(!showRefStats)}>
+                                        {(showRefStats) ? "Hide" : "Expand"}
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </CardHeader>
+                        <Collapse isOpen={showRefStats}>
+                            <CardBody>
+                                <ReferralStatistics/>
+                            </CardBody>
+                        </Collapse>
+                    </Card>
                 </div>
 
             </div>
