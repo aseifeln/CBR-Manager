0cbcf46e49014a10f46fd2fdc17f92a01a1cb3e0
Christofer Calvin Kurniawan <ckurniaw@sfu.ca>
2021-04-08 15:58:37 -0700

    Add visit statistic card
    Statistic is still for referrals

--- client/src/pages/AdminPages/AdminInsights.js
+++ client/src/pages/AdminPages/AdminInsights.js
@@ -3,10 +3,12 @@ import { Button, Card, CardHeader, CardBody, Collapse, Row, Col } from 'reactstr
 import AdminSideBar from '../../components/AdminSideBar';
 import CookieChecker from '../../components/CookieChecker';
 import ReferralStatistics from '../../components/statistics/ReferralStatistics';
+import VisitStatistics from '../../components/statistics/VisitStatistics';
 
 function AdminInsights() {
 
     const [ showRefStats, setShowRefStats ] = useState(false);
+    const [ showVisitStats, setShowVisitStats ] = useState(false);
     const statFontSize = {color:"white",fontSize: "20px", fontWeight: "bold"};
 
     return(
@@ -31,6 +33,23 @@ function AdminInsights() {
                         <Collapse isOpen={showRefStats}>
                             <CardBody>
                                 <ReferralStatistics/>
+                            </CardBody>
+                        </Collapse>
+                    </Card>
+                    <Card>
+                        <CardHeader>
+                            <Row>
+                                <Col><h2 style={{statFontSize}}>Visit Stats</h2></Col>
+                                <Col>
+                                    <Button variant="primary" size="md" className="float-right" onClick={() => setShowVisitStats(!showVisitStats)}>
+                                        {(showVisitStats) ? "Hide" : "Expand"}
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </CardHeader>
+                        <Collapse isOpen={showVisitStats}>
+                            <CardBody>
+                                <VisitStatistics/>
                             </CardBody>
                         </Collapse>
                     </Card>
