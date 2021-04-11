715d8184770d83eafcd4624a7e28070e89b0ea90
Ahmed Seifelnasr <aseifeln@sfu.ca>
2021-04-12 01:12:28 +0200

    Apply code review styling suggestions

--- client/src/components/statistics/CBRPerformance.js
+++ client/src/components/statistics/CBRPerformance.js
@@ -54,21 +54,25 @@ function CBRPerformance() {
 
     return (
         <Container>
-            <div style={{height: '400px', marginBottom: '50px'}}>
-                <h5 style={{textAlign: 'center'}}><b>Top 5 CBR Workers Carrying The Most Visits</b></h5>
-                <BarChart data={mostVisits} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+            <div class="admin-card" data-title="Top 5 CBR Workers Carrying The Most Visits" style={{marginTop: '50px'}}>
+                <div style={{height: '400px'}}>
+                    <BarChart data={mostVisits} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+                </div>
             </div>
-            <div style={{height: '400px', marginBottom: '50px'}}>
-                <h5 style={{textAlign: 'center'}}><b>Top 5 CBR Workers Carrying The Least Visits</b></h5>
-                <BarChart data={leastVisits} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+            <div class="admin-card" data-title="Top 5 CBR Workers Carrying The Least Visits" style={{marginTop: '50px'}}>
+                <div style={{height: '400px'}}>
+                    <BarChart data={leastVisits} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+                </div>
             </div>
-            <div style={{height: '400px', marginBottom: '50px'}}>
-                <h5 style={{textAlign: 'center'}}><b>Top 5 CBR Workers Carrying The Most Referrals</b></h5>
-                <BarChart data={mostReferrals} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+            <div class="admin-card" data-title="Top 5 CBR Workers Carrying The Most Referrals" style={{marginTop: '50px'}}>
+                <div style={{height: '400px'}}>
+                    <BarChart data={mostReferrals} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+                </div>
             </div>
-            <div style={{height: '400px', marginBottom: '50px'}}>
-                <h5 style={{textAlign: 'center'}}><b>Top 5 CBR Workers Carrying The Least Referrals</b></h5>
-                <BarChart data={leastReferrals} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+            <div class="admin-card" data-title="Top 5 CBR Workers Carrying The Least Referrals" style={{marginTop: '50px'}}>
+                <div style={{height: '400px'}}>
+                    <BarChart data={leastReferrals} keys={['Count']} keyAttr="Count" groupBy="Worker" xAxisLabel="Worker" yAxisLabel="Count"/>
+                </div>
             </div>
         </Container>
     )
